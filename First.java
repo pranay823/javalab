86143277a1c19f98c1b138543b747c616a53f0ed
pranay823 <102896608+pranay823@users.noreply.github.com>
2023-03-14 11:14:57 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+class First{
+    int a,b;
+    public static void main(String[]args){
+        First f1=new First();
+        f1.a=34;
+        f1.b=64;
+       System.out.println(f1.a+" "+f1.b);
+   
+        
+    }
+}
